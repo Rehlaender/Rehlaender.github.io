af84781c958f9ba6f32db5182f38b8bb542e3622
rehlaender <avilamario16@gmail.com>
2019-02-28 16:03:59 -0600

    added a random yeehaw

--- scripts/onRender.js
+++ scripts/onRender.js
@@ -6,11 +6,6 @@ var HACKS_SECTION = 'hacksSection';
 var OPEN = 'open';
 var CLOSED = 'closed';
 
-function loadApp() {
-    setSection(ART_SECTION, CLOSED);
-    setSection(CODE_SECTION, CLOSED);
-}
-
 function setSection(section, setState) {
     document.getElementById(section).setAttribute('data-state', setState);
 }
@@ -37,4 +32,66 @@ function closeArtSection() {
 function closeCodeSection() {
     console.log('help')
     setSection(CODE_SECTION, CLOSED);
+}
+
+// min-max
+// -1000 ~ 5000
+var positionX = '0';
+// -1000 ~ 5000
+var positionY = '0';
+// 300 ~ 3000
+var speed = '300';
+
+function generateRandomNumber(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function generateRandomXPosition() {
+    return generateRandomNumber(2000, -500);
+}
+
+function generateRandomYPosition() {
+    return generateRandomNumber(500, -500);
+}
+
+function generateRandomSpeed() {
+    return generateRandomNumber(5000, 1000);
+}
+
+function setRandomText() {
+    setRandomText1();
+    setRandomText2();
+}
+
+function setRandomText1() {
+    speed = generateRandomSpeed();
+    positionX = generateRandomXPosition();
+    positionY = generateRandomYPosition();
+    var stupidText = document.getElementById('stupidText');
+    stupidText.style.top = positionY + 'px';
+    stupidText.style.left = positionX + 'px';
+    stupidText.style.transitionDuration = speed + 'ms';
+    console.log('set time', speed);
+}
+
+function setRandomText2() {
+    speed = generateRandomSpeed();
+    positionX = generateRandomXPosition();
+    positionY = generateRandomYPosition();
+    var stupidText = document.getElementById('stupidText2');
+    stupidText.style.top = positionY + 'px';
+    stupidText.style.left = positionX + 'px';
+    stupidText.style.transitionDuration = speed + 'ms';
+    console.log('set time', speed);
+}
+ 
+function tickClockForRandomGeneriation() {
+    window.setInterval(setRandomText, 1200);
+}
+
+// load app
+function loadApp() {
+    tickClockForRandomGeneriation();
+    setSection(ART_SECTION, CLOSED);
+    setSection(CODE_SECTION, OPEN);
 }
