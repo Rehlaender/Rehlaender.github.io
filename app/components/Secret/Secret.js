42182881dd570f02622279404cb0e6e9602a1b65
unknown <avilamario16@gmail.com>
2019-08-22 09:33:16 -0500

    add svg animation

--- app/components/Secret/Secret.js
+++ app/components/Secret/Secret.js
@@ -4,19 +4,31 @@ import Typist from 'react-typist';
 import { Store } from '../../Store';
 
 import {Sign} from './Sign';
+import vector from './secret_animation.svg';
+import vector2 from './secret_air.svg';
 
 export default function Secret() {
   const { state, dispatch } = React.useContext(Store);
 
   return !state.menuState && state.activeModals.length > 0 ? (
-      <div id="main">
-        <Typist>
-          <p>Hello, you discovered the secret</p>
-          <br/>
-          <p>actually there is no secret at all</p>
-          <br/>
-          <p>you did it, now you can sign my wall</p>
-        </Typist>
+      <div id="secret">
+        <h1>wow! secret</h1>
+        <div className="message">
+          <Typist>
+            Hello, you discovered the secret
+            <Typist.Delay ms={1250} />
+            <br />
+            actually there is no secret at all
+            <Typist.Delay ms={1250} />
+            <br />
+            you did it, now you can sign my wall
+          </Typist>
+          <div className="secretCanvas">
+            <embed src={vector}/>
+            <embed src={vector2}/>
+          </div>
+
+        </div>
         <Sign />
       </div>
   ) : null
