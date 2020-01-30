15395a0561440997e45e6c8a225c1ae5ee3b5a17
unknown <avilamario16@gmail.com>
2020-01-30 17:31:33 -0600

    add code drawer

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,22 @@
 // load app
-function loadApp() {
-  console.log('loaded')
+
+function setCodeButtonListener() {  
+  document.addEventListener('click', function (event) {
+  if (!event.target.matches('#codeButton')) return;
+    event.preventDefault();  
+    toggleMenu();
+  }, false);
 }
+
+function toggleMenu() {
+  document.getElementById('codeMenu').classList.toggle('hidden');
+}
+
+function loadApp() {
+  
+  setCodeButtonListener();
+}
+
+window.onload = (event) => {
+  loadApp();
+};
